452ba44260077624b697e4475be46c96b30411a0
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-02 19:41:27 -0400

    Created two funcs two submit the images to the api

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,16 @@
 import React from "react";
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
-    <div>
+   <div>
       <p className="f3 ">
         {"This Magic Brain will detect faces in your pictures. Give it a try. "}
       </p>
       <div className="center">
         <div className="center form pa4 br3 shadow-5">
-          <input type="text" className="f4 pa2 w-70 center" />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange}/>
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit}>
             Detect
           </button>
         </div>
