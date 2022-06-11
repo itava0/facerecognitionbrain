1cef8fa22098a2367992db06ee3a6b8a98d50199
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-06-11 12:18:45 -0400

    Added some error hangle incase the form is empty

--- src/App.js
+++ src/App.js
@@ -101,20 +101,7 @@ const initialState = {
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: "",
-      imageUrl: "",
-      box: {},
-      route: "signin",
-      isSignedIn: false,
-      user: {
-        id: "",
-        name: "",
-        email: "",
-        entries: 0,
-        joined: "",
-      },
-    };
+    this.state = initialState
   }
 
   loadUser = (data) => {
@@ -168,7 +155,7 @@ class App extends Component {
             .then((response) => response.json())
             .then((count) => {
               this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            }).catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
@@ -177,7 +164,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === "signout") {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     } else if (route === "home") {
       this.setState({ isSignedIn: true });
     }
